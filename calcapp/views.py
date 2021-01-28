d85f6da13d198e6088d1ce44ff51342f8f7a0d75
alexbprofit <you@example.com>
2021-01-28 12:34:06 +0200

    add posts in main page

--- calcapp/views.py
+++ calcapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-# Create your views here.
+from django.utils import timezone
+from .models import Post
+
 
 def post_list(request):
-    return render(request, 'calcapp/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'calcapp/post_list.html', {'posts': posts})
